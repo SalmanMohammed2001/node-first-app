27cd4dcef7e7a2a6ae2900ff7a536912b970b60d
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-25 00:10:41 +0530

    set env port

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express=require('express')
 const mongoose=require('mongoose')
 const app=express();
 
+require('dotenv').config();
+
+const port=process.env.SERVER_PORT || 3000
+
 mongoose.connect('mongodb://127.0.0.1:27017/customer_crud2').then(()=>{
-    app.listen(3000,()=>{
+    app.listen(port,()=>{
         console.log('api started end running on port 3000')
     })
+})
+
+app.use("/",(req,res,next)=>{
+    res.send('<h1>salman</h1')
 })
