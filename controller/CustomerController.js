5918a799069010fb5610e3f207ede92ad4c201be
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-25 15:54:21 +0530

    CustomerController find ok

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -2,7 +2,6 @@ const Customer=require('../model/Customerschema')
 
 
 const saveCustomer=(req,res)=>{
-
     const tempCustomer=new Customer({
             nic:req.body.nic,
             name:req.body.name,
@@ -11,10 +10,32 @@ const saveCustomer=(req,res)=>{
     });
     tempCustomer.save().then((result)=>{
         res.status(200).json({status:true,message:'customer saved!',data:result})
+    }).catch((error)=>{
+        res.status(500).json(error)
     })
 }
-const findCustomer=(req,res)=>{}
-const updateCustomer=(req,res)=>{}
-const deleteCustomer=(req,res)=>{}
-const findAllCustomer=(req,res)=>{}
+const findCustomer=(req,res)=>{
+    Customer.findOne({nic:req.params.nic}).then((result)=>{
+        if(result==null){
+            res.status(404).json({status:false,message:'customer not found'})
+        }else {
+            res.status(200).json({status:true,data:result})
+        }
+    })
+}
+
+
+const updateCustomer=(req,res)=>{
+
+}
+
+
+const deleteCustomer=(req,res)=>{
+
+}
+
+
+const findAllCustomer=(req,res)=>{
+
+}
 
