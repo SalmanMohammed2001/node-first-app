a77173d1a9878360b23ec1474949d65ff61cdf71
salmanMohammed2001 <salmannisthar123@gmail.com>
2023-12-26 14:24:45 +0530

    add use login step1 ok

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,4 +1,5 @@
 const UserSchema=require('../model/UserSchema')
+const bcrypt=require('bcrypt')
 const e = require("express");
 
 const signup= async (req,res)=>{
@@ -6,27 +7,56 @@ const signup= async (req,res)=>{
 
         console.log(req.body)
 
-       if(result==null){
-           console.log(req.body)
-            const user= UserSchema({
-                username:req.body.username,
-                fullName:req.body.fullName,
-                password:req.body.password
-            });
-            user.save().then(saveData=>{
-                res.status(200).json({message:'user saved!',data:saveData})
-            }).catch(err=>{
-                res.status(500).json(err)
-            })
-        }else {
-            res.status(409).json({message:'email already exist'})
-        }
+        bcrypt.hash(req.body.password, 10, function(err, hash) {
+            // Store hash in your password DB.
+
+            if(err){
+                res.status(500).json({message:'something went wrong'})
+            }
+
+            if(result==null){
+                console.log(req.body)
+                const user= UserSchema({
+                    username:req.body.username,
+                    fullName:req.body.fullName,
+                    password:hash
+                });
+                user.save().then(saveData=>{
+                    res.status(200).json({message:'user saved!',data:saveData})
+                }).catch(err=>{
+                    res.status(500).json(err)
+                })
+            }else {
+                res.status(409).json({message:'email already exist'})
+            }
+
+        });
+
+
+
 
     }).catch(error=>{
         res.status(500).json(error)
     })
 }
-const login= async (req,res)=>{}
+const login= async (req,res)=>{
+    UserSchema.findOne({username:req.body.username}).then((selectedUser)=>{
+        if(selectedUser===null){
+            return res.status(404).json({message:'user name not found!'})
+        }else {
+
+            bcrypt.compare(req.body.password, selectedUser.password, function(err, result) {
+
+                if(result){
+                    res.status(200).json({message:'login',data:result})
+                }else {
+                    return res.status(401).json({message:'password InCrate'})
+                }
+            });
+
+        }
+    })
+}
 
 
 module.exports={
